695f8d918dcb99a2050446e502fca6fd67b4a9a2
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-02-20 20:41:12 +0530

    again changes

--- initializers.cpp
+++ initializers.cpp
@@ -16,9 +16,13 @@ int main(){
     std::cout<<age<<std::endl<<percentage<<std::endl<<number<<std::endl;
 
     // can use expression as initializer
-
+    // it can be possible in first bracket also
     int total {age + percentage};
 
+    int anotherTotal (age+number);
+
+
     std::cout<<"total :"<<total<<std::endl;
+    std::cout<<"total :"<<anotherTotal<<std::endl;
     return 0;
 }
