2913e85c87e9c74c2788ced08e80c491c559cda7
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-02-22 19:14:01 +0530

    added character file

---
+++ character.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+int main(){
+    char character1 {'a'};
+    char character2 {'r'};
+    char character3 {'r'};
+    char character4 {'o'};
+    char character5 {'w'};
+
+    std::cout<<character1<<std::endl;
+    std::cout<<character2<<std::endl;
+    std::cout<<character3<<std::endl;
+    std::cout<<character4<<std::endl;
+    std::cout<<character5<<std::endl;
+    
+    std::cout<<"size of char : "<<sizeof(char)<<std::endl;
+
+    std::cout<<std::endl;
+    
+    char value = 65;
+
+    std::cout<<"value : "<<value<<std::endl;
+    std::cout<<"value(int) : "<<static_cast<int>(value)<<std::endl;
+
+    return 0;
+}
