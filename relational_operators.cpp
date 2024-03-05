a07869e6e3a13839687d22110025658121c24861
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-03-05 23:47:43 +0530

    changed

--- relational_operators.cpp
+++ relational_operators.cpp
@@ -4,6 +4,10 @@ int main(){
     int a {45};
     int b {70};
 
+    std::cout<<"number1 : "<<a<<std::endl;
+    std::cout<<"number2 : "<<b<<std::endl;
+    
+
     std::cout<<std::boolalpha;  // to convert boolean outputs from 0 and 1 to TRUE or FALSE
 
     std::cout<<"number1 < number2 : "<<(a<b)<<std::endl;
