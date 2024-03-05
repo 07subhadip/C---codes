6b93d151d56cf8ac6ca10973a8d1636ea53b114b
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-03-05 23:44:16 +0530

    code added

--- relational_operators.cpp
+++ relational_operators.cpp
@@ -1,6 +1,18 @@
 #include<iostream>
 
 int main(){
-    
+    int a {45};
+    int b {70};
+
+    std::cout<<std::boolalpha;  // to convert boolean outputs from 0 and 1 to TRUE or FALSE
+
+    std::cout<<"number1 < number2 : "<<(a<b)<<std::endl;
+    std::cout<<"number1 > number2 : "<<(a>b)<<std::endl;
+    std::cout<<"number1 <= number2 : "<<(a<=b)<<std::endl;
+    std::cout<<"number1 >= number2 : "<<(a>=b)<<std::endl;
+    std::cout<<"number1 == number2 : "<<(a==b)<<std::endl;
+    std::cout<<std::noboolalpha;    // opposite of boolalpha
+    std::cout<<"number1 != number2 : "<<(a!=b)<<std::endl;
+
     return 0;
 }
