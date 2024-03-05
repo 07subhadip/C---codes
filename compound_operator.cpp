0ed871ff007d29dc9cc36e68dcab22375fb9a5e0
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-03-05 23:36:50 +0530

    added code

--- compound_operator.cpp
+++ compound_operator.cpp
@@ -1,6 +1,22 @@
 #include<iostream>
 
 int main(){
-    
+    int value {10};
+
+    value+=5;
+    std::cout<<"value+=5 : "<<value<<std::endl;
+
+    value-=7;
+    std::cout<<"value-=7 : "<<value<<std::endl;
+
+    value*=3;
+    std::cout<<"value*=3 : "<<value<<std::endl;
+
+    value/=2;
+    std::cout<<"value/=2 : "<<value<<std::endl;
+
+    value%=5;
+    std::cout<<"value%=5 : "<<value<<std::endl;
+
     return 0;
 }
