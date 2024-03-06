7fc16e0052b18cc813a33150dbdd5889c7544a85
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-03-06 23:43:56 +0530

    added

--- logical_operators.cpp
+++ logical_operators.cpp
@@ -22,6 +22,16 @@ int main(){
     std::cout<<"a || b : "<<(a||b)<<std::endl;
     std::cout<<"a || c : "<<(a||c)<<std::endl;
     std::cout<<"a || b || c : "<<(a||b||c)<<std::endl;
-    
+
+    std::cout<<std::endl;
+    std::cout<<"**************Basic NOT operators**************"<<std::endl;
+    std::cout<<"!a : "<<(!a)<<std::endl;
+    std::cout<<"!b : "<<(!b)<<std::endl;
+    std::cout<<"!c : "<<(!c)<<std::endl;
+
+
+    std::cout<<std::endl;
+    std::cout<<"Combining all operators"<<std::endl;
+
     return 0;
 }
