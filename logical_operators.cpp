7d4b2056fa157323c5ac06d4b479c67af057947e
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-03-06 23:39:55 +0530

    AND OR added

--- logical_operators.cpp
+++ logical_operators.cpp
@@ -1,6 +1,27 @@
 #include<iostream>
 
 int main(){
+    bool a{true};
+    bool b{false};
+    bool c{true};
+    std::cout<<std::boolalpha;
+    std::cout<<"a : "<<a<<std::endl;
+    std::cout<<"b : "<<b<<std::endl;
+    std::cout<<"c : "<<c<<std::endl;
+
+    std::cout<<std::endl;
+    std::cout<<"***************Basic AND operators***************"<<std::endl;
+
+    std::cout<<"a && b : "<<(a&&b)<<std::endl;
+    std::cout<<"a && c : "<<(a&&c)<<std::endl;
+    std::cout<<"a && b && c : "<<(a&&b&&c)<<std::endl;
+
+
+    std::cout<<std::endl;
+    std::cout<<"**************Basic OR operators**************"<<std::endl;
+    std::cout<<"a || b : "<<(a||b)<<std::endl;
+    std::cout<<"a || c : "<<(a||c)<<std::endl;
+    std::cout<<"a || b || c : "<<(a||b||c)<<std::endl;
     
     return 0;
 }
