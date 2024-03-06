8a444076e7071e47793e010b9fe26a0b87f7900a
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-03-06 23:48:50 +0530

    added

--- logical_operators.cpp
+++ logical_operators.cpp
@@ -4,6 +4,8 @@ int main(){
     bool a{true};
     bool b{false};
     bool c{true};
+
+    std::cout<<"************************************************"<<std::endl<<std::endl;
     std::cout<<std::boolalpha;
     std::cout<<"a : "<<a<<std::endl;
     std::cout<<"b : "<<b<<std::endl;
@@ -11,6 +13,8 @@ int main(){
 
     std::cout<<std::endl;
     std::cout<<"***************Basic AND operators***************"<<std::endl;
+    std::cout<<std::endl;
+
 
     std::cout<<"a && b : "<<(a&&b)<<std::endl;
     std::cout<<"a && c : "<<(a&&c)<<std::endl;
@@ -19,19 +23,27 @@ int main(){
 
     std::cout<<std::endl;
     std::cout<<"**************Basic OR operators**************"<<std::endl;
+    std::cout<<std::endl;
+
     std::cout<<"a || b : "<<(a||b)<<std::endl;
     std::cout<<"a || c : "<<(a||c)<<std::endl;
     std::cout<<"a || b || c : "<<(a||b||c)<<std::endl;
 
     std::cout<<std::endl;
     std::cout<<"**************Basic NOT operators**************"<<std::endl;
+    std::cout<<std::endl;
+
     std::cout<<"!a : "<<(!a)<<std::endl;
     std::cout<<"!b : "<<(!b)<<std::endl;
     std::cout<<"!c : "<<(!c)<<std::endl;
 
 
     std::cout<<std::endl;
-    std::cout<<"Combining all operators"<<std::endl;
+    std::cout<<"************Combining all operators************"<<std::endl;
+    std::cout<<std::endl;
+
+    std::cout<<"!(a && b) || c : "<<(!(a && b)|| c)<<std::endl;
+
 
     return 0;
 }
