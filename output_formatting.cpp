dc20e3168b903da31fc81cd59d7073594313063e
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-03-07 23:15:35 +0530

    justified

--- output_formatting.cpp
+++ output_formatting.cpp
@@ -22,9 +22,13 @@ int main(){
     std::cout<<"Anusuya"<<" "<<"Ghosal"<<"22"<<std::endl;
     std::cout<<"Mithu"<<" "<<"Hensh"<<"42"<<std::endl;
     std::cout<<"Dipak"<<" "<<"Hensh"<<"44"<<std::endl;
+       
+    std::cout<<"****************************************"<<std::endl<<std::endl;
 
     std::cout<<std::endl;
     std::cout<<"Formatted Table"<<std::endl<<std::endl;
+
+    std::cout<<"****************************************"<<std::endl<<std::endl;
 
     std::cout<<std::setw(10)<<"Firstname"<<std::setw(10)<<"Lastname"<<std::setw(7)<<"Age"<<std::endl<<std::endl;
     std::cout<<std::setw(10)<<"Subhadip"<<std::setw(10)<<"Hensh"<<std::setw(7)<<"23"<<std::endl;
@@ -32,6 +36,8 @@ int main(){
     std::cout<<std::setw(10)<<"Mithu"<<std::setw(10)<<"Hensh"<<std::setw(7)<<"42"<<std::endl;
     std::cout<<std::setw(10)<<"Dipak"<<std::setw(10)<<"Hensh"<<std::setw(7)<<"44"<<std::endl;
     std::cout<<std::endl;
+
+    std::cout<<"****************************************"<<std::endl<<std::endl;
 
     std::cout<<"Formatted Table with variable"<<std::endl<<std::endl;
     
@@ -43,6 +49,15 @@ int main(){
     std::cout<<std::setw(col_width)<<"Mithu"<<std::setw(col_width)<<"Hensh"<<std::setw(col_width)<<"42"<<std::endl;
     std::cout<<std::setw(col_width)<<"Dipak"<<std::setw(col_width)<<"Hensh"<<std::setw(col_width)<<"44"<<std::endl;
 
+    std::cout<<"****************************************"<<std::endl<<std::endl;
+
+    //justified to the left
+    std::cout<<std::left;
+    std::cout<<std::setw(col_width)<<"Firstname"<<std::setw(col_width)<<"Lastname"<<std::setw(col_width)<<"Age"<<std::endl<<std::endl;
+    std::cout<<std::setw(col_width)<<"Subhadip"<<std::setw(col_width)<<"Hensh"<<std::setw(col_width)<<"23"<<std::endl;
+    std::cout<<std::setw(col_width)<<"Anusuya"<<std::setw(col_width)<<"Ghosal"<<std::setw(col_width)<<"22"<<std::endl;
+    std::cout<<std::setw(col_width)<<"Mithu"<<std::setw(col_width)<<"Hensh"<<std::setw(col_width)<<"42"<<std::endl;
+    std::cout<<std::setw(col_width)<<"Dipak"<<std::setw(col_width)<<"Hensh"<<std::setw(col_width)<<"44"<<std::endl;
 
     return 0;
 }
