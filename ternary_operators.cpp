8fde2f940b19bcca3548f360a8ca492edb9c48fe
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-03-10 23:56:38 +0530

    init

--- ternary_operators.cpp
+++ ternary_operators.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
 
 int main(){
-    
+    // result = condition ? result1 : result2 ;
+
+    int max{};
+
+    int a{45};
+    int b{50};
+
+    max = (a<b)?a:b;
+
+    std::cout<<"Result : "<<max<<std::endl;
     return 0;
 }
