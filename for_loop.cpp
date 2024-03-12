db6b137c08efb419f59f9e06fdf2c5f6db69654e
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-03-12 22:53:48 +0530

    added

--- for_loop.cpp
+++ for_loop.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
 
 int main(){
+    for(unsigned int i{0}; i<10 ;++i){
+        std::cout<<i<<" : I Love C++"<<std::endl;
+    }
+    std::cout<<"Loop Ended"<<std::endl;
     
     return 0;
 }
