01ee1a03386be03bd351d0a33085042a7043deee
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-03-12 23:13:21 +0530

    updated

--- for_loop.cpp
+++ for_loop.cpp
@@ -13,5 +13,18 @@ int main(){
     }
     std::cout<<"Loops Done ! "<<std::endl;
 
+    std::cout<<std::endl<<"Size of (size_t) : "<<sizeof(size_t)<<std::endl<<std::endl;
+
+    // declare values in variables
+    size_t i {0};
+    const size_t COUNT {10};
+
+    for(;i<COUNT ; ++i ){
+        std::cout<<i<<" : I Love C++"<<std::endl;
+    }
+    std::cout<<"Loop Done ! "<<std::endl;
+
+
+
     return 0;
 }
