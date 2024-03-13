f8ea563aad8a6e0857ab2b1b77b90002a9146032
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-03-13 08:12:46 +0530

    updated

--- do_while_loop.cpp
+++ do_while_loop.cpp
@@ -2,5 +2,12 @@
 
 int main(){
 
+    const size_t COUNT{10};
+    size_t i{0};
+
+    do{
+        std::cout<<i<<" : I Love C++"<<std::endl;       // this line iterates minimum one time even if the condition is false
+        i++;
+    }while(i<COUNT);
     return 0;
 }
