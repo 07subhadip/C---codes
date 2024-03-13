c14e2ef32af64f478c617e9d17a26546f2cac810
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-03-13 08:01:30 +0530

    updated

--- while_loop.cpp
+++ while_loop.cpp
@@ -1,6 +1,12 @@
 #include<iostream>
 
 int main(){
+    const size_t COUNT{10};
+    size_t i {0};
 
+    while(i<COUNT){
+        std::cout<<i<<" : I Love C++"<<std::endl;
+        i++;
+    }
     return 0;
 }
