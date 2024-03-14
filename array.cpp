6d86eb35979cef60aa3468c18ee7f15920b61193
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-03-14 09:02:34 +0530

    updated

--- array.cpp
+++ array.cpp
@@ -1,5 +1,4 @@
 #include<iostream>
-
 int main(){
     int score[10];
 
@@ -34,5 +33,33 @@ int main(){
     for(auto value : class_size){
         std::cout<<"class_size [ ] : "<<value<<std::endl;
     }
+
+    //sum up array
+    std::cout<<std::endl<<std::endl;
+
+    const int birds[]{27, 51, 14, 89, 33, 72, 45, 60, 98, 22};
+    int sum=0;
+    for(int element:birds){
+        sum+=element;
+    }
+
+    std::cout<<"Sum of the array is : "<<sum<<std::endl;
+
+    std::cout<<std::endl<<std::endl;
+    
+
+    int array[]{27, 51, 14, 89, 33, 72, 45, 60, 98, 22};
+
+    // int Count{std::size(array)};    this line is only for C++17 before that to get the size of the array sizeof function is used
+    
+    std::cout<<"Size of array : "<<sizeof(array)<<std::endl;
+    std::cout<<"Size of element in array : "<<sizeof(array[0])<<std::endl;
+    
+    int count{sizeof(array)/sizeof(array[0])};
+    for(size_t i{0} ; i<count;i++){
+        std::cout<<"Array ["<<i<<"] : "<<array[i]<<std::endl;
+    }
+
+
     return 0;
 }
