949d03043500e31c14f713ee3ae5d96424d8f2b0
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-03-14 23:34:56 +0530

    updated

--- array_of_characters.cpp
+++ array_of_characters.cpp
@@ -1,6 +1,18 @@
 #include<iostream>
 
 int main(){
-    
+    char message[]{'H','e','l','l','o','\0'};   // \0 is used to indicate that the array has been ended aka null termination character
+    for(auto i:message){
+        std::cout<<i;
+    }
+    std::cout<<std::endl;
+    std::cout<<message<<std::endl;
+
+    char name[]{'s','u','b','h','a','\0'};
+    std::cout<<name<<std::endl;
+    std::cout<<"sizeof(name) : "<<sizeof(name)<<std::endl;
+    char name1[6]{'s','u','b','h','a'};     // instead of adding the null terminator character we can do this
+    std::cout<<name1<<std::endl;
+    std::cout<<"sizeof(name1) : "<<sizeof(name1)<<std::endl;
     return 0;
 }
