ddb61c0b0636daf790c970e6118f513cf6b6d508
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-03-15 09:10:10 +0530

    updated

--- bound_of_array.cpp
+++ bound_of_array.cpp
@@ -1,6 +1,12 @@
 #include<iostream>
 
 int main(){
-
+    int arr[]{1,2,3,4,5};
+    // Read beyond bound : will print garbage value or crash the program
+    std::cout<<"arr[10] : "<<arr[10]<<std::endl;
+    //program will crash here line(8-10)
+    arr[104324234] = 1000;
+    std::cout<<"arr[1104324234] : "<<arr[104324234];
+    std::cout<<"program ended..."<<std::endl;
     return 0;
 }
