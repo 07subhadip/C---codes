ac3a899c3392c99b2a5a1e776c5c21c701959a30
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-03-16 10:21:04 +0530

    updated

--- pointers/pointer_to_char.cpp
+++ pointers/pointer_to_char.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
 
 int main(){
-    
+    // to not modify the string use this
+    const char* message{"Hello world!"};  // the Hello World is ocnverted to const char array that's why we need to set a const char pointer for this
+    std::cout<<"message : "<<message<<std::endl;
+    std::cout<<"*message : "<<*message<<std::endl;  // this will print the first character in the const char array in this case which is H
+
+    char message2[]{"Hello World!"};    // to modify the string use this
+    message2[0]='B';
+    std::cout<<"message2 : "<<message2<<std::endl;
     return 0;
 }
