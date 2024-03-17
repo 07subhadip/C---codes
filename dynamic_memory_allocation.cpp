e6931127fd150b10a91a43c7364e35040a9157a3
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-03-17 23:40:42 +0530

    updated

---
+++ dynamic_memory_allocation.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main(){
+
+    // this code will crash due to not initialize pointer correctly
+    // int* var_ptr;
+    // *var_ptr = 55;       here the program is crashing
+
+    // std::cout<<"var_ptr : "<<var_ptr<<std::endl;
+    // std::cout<<"*var_ptr : "<<*var_ptr<<std::endl;
+
+
+
+    return 0;
+}
