478f2e8bc0e37bc220b79a48b1c9a11166f54f0e
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-03-22 08:20:44 +0530

    updated

--- pointers/references.cpp
+++ pointers/references.cpp
@@ -1,6 +1,53 @@
 #include<iostream>
 
 int main(){
+    int int_data{33};
+    double double_data{55};
+
+    // referencing
+
+    int& ref_int_data{int_data};
+    double& ref_double_data{double_data};
+
+    std::cout<<"int_data : "<<int_data<<std::endl;
+    std::cout<<"&int_data : "<<&int_data<<std::endl;
+    std::cout<<"double_data : "<<double_data<<std::endl;
+    std::cout<<"&double_data : "<<&double_data<<std::endl;
+
+    std::cout<<"ref_int_data : "<<ref_int_data<<std::endl;
+    std::cout<<"&ref_int_data : "<<&ref_int_data<<std::endl;
+    std::cout<<"ref_double_data : "<<ref_double_data<<std::endl;
+    std::cout<<"&ref_double_data : "<<&ref_double_data<<std::endl;
+    
+    std::cout<<"*****************************************"<<std::endl;
+    int_data = 111;
+    double_data = 43.43;
+
+    std::cout<<"int_data : "<<int_data<<std::endl;
+    std::cout<<"&int_data : "<<&int_data<<std::endl;
+    std::cout<<"double_data : "<<double_data<<std::endl;
+    std::cout<<"&double_data : "<<&double_data<<std::endl;
+
+    std::cout<<"ref_int_data : "<<ref_int_data<<std::endl;
+    std::cout<<"&ref_int_data : "<<&ref_int_data<<std::endl;
+    std::cout<<"ref_double_data : "<<ref_double_data<<std::endl;
+    std::cout<<"&ref_double_data : "<<&ref_double_data<<std::endl;
+
+
+    std::cout<<"*****************************************"<<std::endl;
+
+    ref_int_data = 1002;
+    ref_double_data = 234.345;
+    
+    std::cout<<"int_data : "<<int_data<<std::endl;
+    std::cout<<"&int_data : "<<&int_data<<std::endl;
+    std::cout<<"double_data : "<<double_data<<std::endl;
+    std::cout<<"&double_data : "<<&double_data<<std::endl;
+
+    std::cout<<"ref_int_data : "<<ref_int_data<<std::endl;
+    std::cout<<"&ref_int_data : "<<&ref_int_data<<std::endl;
+    std::cout<<"ref_double_data : "<<ref_double_data<<std::endl;
+    std::cout<<"&ref_double_data : "<<&ref_double_data<<std::endl;
 
     return 0;
 }
