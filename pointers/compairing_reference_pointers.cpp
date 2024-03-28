53d514091bb0b81c20c9a2bd59332756d9db2a7a
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-03-28 12:02:02 +0530

    init+update code

--- pointers/compairing_reference_pointers.cpp
+++ pointers/compairing_reference_pointers.cpp
@@ -40,5 +40,31 @@ int main(){
   std::cout<<"Ref_double_value : "<<ref_double_value<<std::endl;
   std::cout<<"p_double_value : "<<p_double_value<<std::endl;
   std::cout<<"*p_double_value : "<<*p_double_value<<std::endl;
+
+  std::cout<<"*******************************************************"<<std::endl<<std::endl;
+
+  // make the pointer point to something else
+
+  p_double_value = &some_other_double;
+
+  std::cout<<"double_Value : "<<double_value<<std::endl;
+  std::cout<<"Ref_double_value : "<<ref_double_value<<std::endl;
+  std::cout<<"&double_value : "<<&double_value<<std::endl;
+  std::cout<<"&Ref_double_value : "<<&ref_double_value<<std::endl;
+  std::cout<<"p_double_value : "<<p_double_value<<std::endl;
+  std::cout<<"*p_double_value : "<<*p_double_value<<std::endl;
+
+
+  std::cout<<"*******************************************************"<<std::endl<<std::endl;
+
+  *p_double_value = 555.55;
+  std::cout<<"double_Value : "<<double_value<<std::endl;
+  std::cout<<"Ref_double_value : "<<ref_double_value<<std::endl;
+  std::cout<<"&double_value : "<<&double_value<<std::endl;    // the original pointer is living here
+  std::cout<<"&Ref_double_value : "<<&ref_double_value<<std::endl;
+  std::cout<<"p_double_value : "<<p_double_value<<std::endl;  // but the pointer is pointing something else
+  std::cout<<"*p_double_value : "<<*p_double_value<<std::endl;
+
+
   return 0;
 }
