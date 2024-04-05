946e0baeef14fffc9cb6d35b13f16ad65d4e896c
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-04-05 19:29:11 +0530

    updated

--- one_definition_rule/one_definition_rule.cpp
+++ one_definition_rule/one_definition_rule.cpp
@@ -2,16 +2,21 @@
 
 // Variable : Definition and Declaration
 
-double weight{20};
+double weight{};
+// double weight{};
 
 double add(double a,double b);
 
 struct point{
-    
-}
+    double m_x{10};
+    double m_y{20};
+};
+
 int main(){
+    point p1;
     std::cout<<weight<<std::endl;
     std::cout<<add(10,20)<<std::endl;
+    std::cout<<"m_x : "<<p1.m_x<<"\nm_y : "<<p1.m_y<<std::endl;
     return 0 ;
 }
 double add(double a,double b){
