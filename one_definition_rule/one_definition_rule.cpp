91fec594d84a931f2a26c7517adf02d8dd556a07
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-04-05 20:03:41 +0530

    created header file and used it

--- one_definition_rule/one_definition_rule.cpp
+++ one_definition_rule/one_definition_rule.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 
+#include "person.h"
+
 // Variable : Definition and Declaration
 
 double weight{};
@@ -17,8 +19,22 @@ int main(){
     std::cout<<weight<<std::endl;
     std::cout<<add(10,20)<<std::endl;
     std::cout<<"m_x : "<<p1.m_x<<"\nm_y : "<<p1.m_y<<std::endl;
+
+    Person person1("Subhadip Hensh",23);        // I had faced a confilct here between p1 and person1
+    person1.print_info();
+
+
+
+
     return 0 ;
 }
 double add(double a,double b){
     return a+b;
 }
+
+int Person::person_count = 8;
+
+Person::Person(const std::string& names_param, int age_param)
+    : full_name{names_param},age{age_param}{
+        ++person_count;
+    }
