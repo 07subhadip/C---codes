cfa402ec692199c15ff7a3d99463619db05fe42e
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-04-11 18:29:00 +0530

    update completed file

--- pass_by_pointer.cpp
+++ pass_by_pointer.cpp
@@ -1,6 +1,16 @@
 #include<iostream>
 
-int main(){
+void say_age(int* age);
 
+int main(){
+    int age{23};
+    std::cout<<"Age before : "<<age<<"    address : "<<&age<<std::endl;
+    say_age(&age);
+    std::cout<<"Sge after : "<<age<<"    address : "<<&age<<std::endl;
     return 0;
+}
+
+void say_age(int* age){
+    ++(*age);
+    std::cout<<"Age at fucntion is : "<<*age<<"    address : "<<age<<std::endl;
 }
