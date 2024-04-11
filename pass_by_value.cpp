7af35606e7e1a3f943ab54f6203682c4b9786764
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-04-11 18:18:48 +0530

    update completed file

--- pass_by_value.cpp
+++ pass_by_value.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
 
+void say_age(int age){
+    // copy age
+    ++age;
+    std::cout<<"Hello, Your are "<<age<<" years old  & age"<<&age<<std::endl;
+    // terminate the copy age
+}
+
 int main(){
 
+    int age{34};
+    std::cout<<"age before : "<<age<<"&age : "<<&age<<std::endl;
+    say_age(age);
+    std::cout<<"age after : "<<age<<"&age : "<<&age<<std::endl;
     return 0;
 }
