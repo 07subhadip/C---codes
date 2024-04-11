32c9dd44e553ef09f20baf7ef0b35ef601f0423e
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-04-11 18:55:13 +0530

    updated completed file

--- pass_by_reference.cpp
+++ pass_by_reference.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
 
-
+void say_age(int& age);
 
 int main(){
-
+    int age{23};
+    std::cout<<"age before : "<<age<<"    address : "<<&age<<std::endl;
+    say_age(age);
+    std::cout<<"age : "<<age<<"    address : "<<&age<<std::endl;
     return 0;
+}
+
+void say_age(int& age){
+    ++age;
+    std::cout<<"age : "<<age<< "    address : "<<&age<<std::endl;
 }
