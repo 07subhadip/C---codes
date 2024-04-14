9ac190a80293433ac3e116b3097d9e78caeb75ed
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-04-14 11:42:31 +0530

    update

--- function_overloading.cpp
+++ function_overloading.cpp
@@ -1,26 +1,41 @@
 #include<iostream>
 #include<string>
+// #include<string_view>        not necessary to use string_view
 // function overloading using different parameters
 
+// function overloading can be created using three forms
+// 1. Number of parameters
+// 2. Datatype Sequence
+// 3. Different Datatype of same name function
+
 int max(int a ,int b){
-    if(a>b)
-    return a;
-    else
-    return b;
+    std::cout<<"Int overload is called"<<std::endl;
+    return(a>b)?a:b;
 }
 
 double max(double a,double b){
-    if(a>b)
-    return a;
-    else
-    return b;
+    std::cout<<"Double overload is called"<<std::endl;
+    return(a>b)?a:b;
 }
 
-std::string max(std::string& a,std::string& b){
-    if(a.size()>b.size())
+double max(int a,double b){
+    std::cout<<"Int Double overload is called"<<std::endl;
+    return(a>b)?a:b;
+}
+
+double max(double a,int b){
+    std::cout<<"Double Int overload is called"<<std::endl;
+    return(a>b)?a:b;
+}
+
+double max(double a,int b,int c){
+    std::cout<<"Double Int Int overload is called"<<std::endl;
     return a;
-    else
-    return b;
+}
+
+std::string_view max(std::string_view a,std::string_view b){
+    std::cout<<"String_view overload is called"<<std::endl;
+    return(a>b)?a:b;
 }
 
 int main(){
@@ -28,12 +43,18 @@ int main(){
     int b{34};
     double a1{23.43};
     double b1{54.76};
-    std::string a2("Hello World");
-    std::string b2("Mississippi");
+    std::string_view a2("Hello C++ World");
+    std::string_view b2("Mississippi");
 
     std::cout<<max(a,b)<<std::endl;
     std::cout<<max(a1,b1)<<std::endl;
     std::cout<<max(a2,b2)<<std::endl;
+    std::cout<<max(a,a1)<<std::endl;
+    std::cout<<max(b1,b)<<std::endl;
+    std::cout<<max(b1,a,23)<<std::endl;
+
+    // even we can do this too
+    std::cout<<max("Hello","Kolkata")<<std::endl;
     
     return 0;
 }
