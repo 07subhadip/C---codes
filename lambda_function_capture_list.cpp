9feb666619e35b5cae7ff068bed37bb4ec4fcf54
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-04-20 18:29:50 +0530

    update

--- lambda_function_capture_list.cpp
+++ lambda_function_capture_list.cpp
@@ -10,5 +10,33 @@ int main(){
         std::cout<<"a + b : "<<a+b<<std::endl;
     };
     func();
+
+    // capturing by value
+
+    int c{10};
+
+    auto func2 = [c](){
+        std::cout<<"C : "<<c<<" Address inner : "<<&c<<std::endl;
+    };
+
+    for(size_t i{};i<=5;i++){
+        std::cout<<"Outer value : "<<c<<" Address out : "<<&c<<std::endl;
+        func2();
+        c++;
+    }
+
+    // capture by reference
+
+    int d{45};
+
+    auto func3 = [&d](){
+        std::cout<<"d : "<<d<<std::endl;
+    };
+
+    for(size_t i{};i<=5;i++){
+        std::cout<<"outer d : "<<d<<std::endl;
+        func3();
+        d++;
+    }
     return 0;
 }
